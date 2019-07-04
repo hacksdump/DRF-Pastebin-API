2cacc610057332a25d6d2c14009f2bffd3b28701
Harshit Maurya <hmaurya999@gmail.com>
2019-07-04 15:04:54 +0530

    Add swagger API docs with stock functionality

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from snippets.views import SnippetViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.views import get_swagger_view
 
 schema_view = get_schema_view(title='Pastebin API')
+swagger_view = get_swagger_view(title='Pastebin API')
 
 router = DefaultRouter()
 router.register(r'snippets', SnippetViewSet)
@@ -12,4 +14,5 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('schema', schema_view),
+    path('swagger', swagger_view),
 ]
