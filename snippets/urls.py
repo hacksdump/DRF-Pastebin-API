6676c30a6e7ba254c692b991b90db5751de971fc
Harshit Maurya <hmaurya999@gmail.com>
2019-07-04 13:58:18 +0530

    Added Core API schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from snippets import views
 from snippets.views import SnippetViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = DefaultRouter()
-router.register(r'snippets', views.SnippetViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'snippets', SnippetViewSet)
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('schema', schema_view),
 ]
