b14a3a87fca229589242353737949af306528c4d
Harshit Maurya <hmaurya999@gmail.com>
2020-04-25 17:34:23 +0530

    add admin

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
